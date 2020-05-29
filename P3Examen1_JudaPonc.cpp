c04b67cbdcad5b8a85df1decfb0653bd9f0ac409
Juda <judaponce14@unitec.edu>
2020-05-29 14:18:47 -0600

    base de menu creada

--- P3Examen1_JudaPonc.cpp
+++ P3Examen1_JudaPonc.cpp
@@ -1,7 +1,83 @@
 #include <iostream>
+#include "Empleado.h"
+#include "Tareas.h"
+#include <vector>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
+vector<Empleado>empleados;
+vector<Tareas> tareas;
+int menu();
 int main(int argc, char** argv) {
+int option=0;
+	
+	while( option != 6 ) {
+		switch( option = menu() ){
+			case 1:{
+				break;
+			}
+			
+			case 2:{
+				
+				break;
+			}
+			
+			case 3:{
+				
+				break;
+			}
+			
+			case 4:{
+				
+				break;
+			}
+			
+			case 5:{
+				
+				break;
+			}
+			
+			case 6:{
+				
+				break;
+			}
+			
+			case 7:{
+				cout<<" Saliendo del programa..."<<endl;
+				exit(0);
+				break;
+			}
+			
+		
+		}	
+	}
+	
 	return 0;
+}
+
+
+int menu(){
+	int opcion=0;
+	while(opcion < 1 || opcion > 7){
+		
+		cout<<"         ***MENU*** "<<endl;
+		cout<<"1. Contratar empleado"<<endl;
+		cout<<"2. Despedir empleado"<<endl;
+		cout<<"3. Listar empleados"<<endl;
+		cout<<"4. Crear tarea"<<endl;
+		cout<<"5. Listar tareas"<<endl;
+		cout<<"6. Iniciar proyecto"<<endl;
+		cout<<"7. Salir"<<endl;
+		cout<<"Ingrese una opcion entre 1 y 7: ";
+			cin>>opcion;
+			cout<<endl;
+			if(opcion >= 1 && opcion <= 7)
+				break;
+			else
+				cout<<endl<<"Opcion incorrecta, elija nuevamente "<<endl;	
+				
+	}//Fin del ciclo while
+	
+	
+	return opcion;
 }
